554682d9dd1105fbef8bcc454649de2f52e06180
FerPascualF50 <pascualefernando@gmail.com>
2022-07-26 19:48:03 -0300

    06-React-Intro-HomeWork-finished

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 export default function Card(props) {
   // acá va tu código
   return (
-  <div>
-    <botton>onClick={onClose}X</botton>
-    <h3>{props.name}</h3>
     <div>
-      <p>MIN</p>
-      <p>{props.min}</p>
+      <button onClick={props.onClose}>X</button>
+      <h3>{props.name}</h3>
+      <div>
+        <p>MIN</p>
+        <p>{props.min}</p>
+      </div>
+      <div>
+        <p>MAX</p>
+        <p>{props.max}</p>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt='Img not found' />
     </div>
-    <div>
-    <p>MAX</p>
-      <p>{props.max}</p>
-    </div>
-    <img src ={'falata lo del deadme, en tepleate stroings'} alt ='Img not found' />
-  </div>
   )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function SearchBar(props) {
+export default function SearchBar({ onSearch }) {
   // acá va tu código
   return (
-  <div>
-    <imput type = 'text'/>
-    <botton onClick={() => onSearch('Buscando ciudad')}>Agregar</botton>
+    <div>
+      <imput type='text' />
+      <botton onClick={() => onSearch('Buscando ciudad')}>Agregar</botton>
 
-  </div>
+    </div>
   )
 };
