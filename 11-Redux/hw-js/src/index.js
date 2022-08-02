0c02804dc226a7873985238ce6069837ba323c56
FerPascualF50 <pascualefernando@gmail.com>
2022-08-02 19:18:02 -0300

    11-Redux-HomeWorkFinished

--- 11-Redux/hw-js/src/index.js
+++ 11-Redux/hw-js/src/index.js
@@ -3,28 +3,32 @@ const contador = require('./reducer');
 const { incremento, decremento } = require('./actions');
 
 // En esta linea creamos nuestro store. Pasandole como parametro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
-
+var valor =  document.querySelector('#valor')
 // Esta funcion nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
-  
+  let actualValue = store.getState().contador;
   // Seteamos el numero obtenido como texto dentro del elemento con id 'valor':
-
+  valor.innerText = actualValue
 }
 
 // Ejecutamos la funcion 'renderContador':
-
-
+renderContador();
 
 // Nos subscribimos al store pasandole la misma funcion. Asi cada vez que llegue una accion, ejecutamos la funcion:
-
+store.subscribe(renderContador);
 
 
 // Por ultimo, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la accion correspondiente:
+let btnSum = document.querySelector('#incremento')
+btnSum.onclick = () => store.dispatch(incremento())
+
+
+let btnResm = document.querySelector('#decremento')
+btnResm.onclick = () => store.dispatch(decremento())
--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -9,6 +9,20 @@ const initialState = {
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENTO:
+        return {
+          contador: state.contador + 1
+        }
+      
+    case DECREMENTO:
+      return {
+        contador: state.contador - 1
+      }
+  
+    default:
+    return state
+  }
   
 }
 
